c7797feadd4b6b24ee54f750979248d742d31d9d
MichaelDaum <daum@michaeldaumconsulting.com>
2015-10-18 11:40:45 +0200

    Item13821: remove dependency on File::MMagic
    ... fixes pontential high memory usage

--- lib/Foswiki/Contrib/Stringifier.pm
+++ lib/Foswiki/Contrib/Stringifier.pm
@@ -11,7 +11,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 
 package Foswiki::Contrib::Stringifier;
@@ -19,21 +19,42 @@ package Foswiki::Contrib::Stringifier;
 use strict;
 use warnings;
 
+use Foswiki::Func ();
 use Foswiki::Contrib::Stringifier::Base;
 our @ISA = qw( Foswiki::Contrib::Stringifier::Base );
-use File::MMagic();
 
-our $mmagic;
-sub mmagic {
-  $mmagic = File::MMagic->new() unless $mmagic;
-  return $mmagic;
+our $types;
+sub _getMimeType {
+  my $fileName = shift;
+
+  my $mimeType;
+
+  if ($fileName && $fileName =~ /\.([^.]+)$/) {
+    my $suffix = $1;
+
+    unless ($types) {
+      $types = Foswiki::Func::readFile($Foswiki::cfg{MimeTypesFileName});
+    }
+
+    if ($types =~ /^([^#]\S*).*?\s$suffix(?:\s|$)/im) {
+      $mimeType = $1;
+    }
+  }
+
+  return $mimeType;
 }
+
+
 
 sub stringFor {
   my ($class, $filename) = @_;
 
   return unless -r $filename;
-  my $mime = mmagic->checktype_filename($filename);
+  my $mime = _getMimeType($filename);
+
+  #print STDERR "no mime for $filename\n" unless $mime;
+  return unless $mime;
+
   my $impl = $class->handler_for($filename, $mime);
 
   #print STDERR "file $filename is a $mime ... using ".($impl||'undef')."\n";
@@ -43,6 +64,7 @@ sub stringFor {
 
   return $plugin->stringForFile($filename);
 }
+
 
 
 1;
--- lib/Foswiki/Contrib/StringifierContrib.pm
+++ lib/Foswiki/Contrib/StringifierContrib.pm
@@ -18,8 +18,8 @@ package Foswiki::Contrib::StringifierContrib;
 use strict;
 use warnings;
 
-our $VERSION = '4.10';
-our $RELEASE = '01 Sep 2015';
+our $VERSION = '4.20';
+our $RELEASE = '18 Oct 2015';
 our $SHORTDESCRIPTION = 'Helper library to stringify binary document formats';
 our $NO_PREFS_IN_TOPIC = 1;
 
